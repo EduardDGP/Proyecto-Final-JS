8f4b49ee019e069b25402af97b4e4a343875231c
Eduard Ginés de la Presa <127982403+EduardDGP@users.noreply.github.com>
2024-02-21 20:03:22 +0100

    Add files via upload

--- CRUD-Estudiantes-main/js/app.js
+++ CRUD-Estudiantes-main/js/app.js
@@ -1,108 +1,43 @@
-// Import the functions you need from the SDKs you need
+// Importación de la función initializeApp desde Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore, collection,addDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-
-// TODO: Add SDKs for Firebase products that you want to use
-
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-
-// Your web app's Firebase configuration
-
+// Configuración de Firebase
 const firebaseConfig = {
-
-    apiKey: "AIzaSyAayWzPZF9fqmw-1aZregPxWY-rCVaReKo",
-
-    authDomain: "js-prcrud.firebaseapp.com",
-
-    databaseURL: "https://js-prcrud-default-rtdb.europe-west1.firebasedatabase.app",
-
-    projectId: "js-prcrud",
-
-    storageBucket: "js-prcrud.appspot.com",
-
-    messagingSenderId: "1002204324745",
-
-    appId: "1:1002204324745:web:a7a335994384cc93b03758"
-
+    apiKey: "AIzaSyA7Pge-QF85I0doZKnHdE7zBG1BWsmXyfg",
+    authDomain: "crud-js-394b8.firebaseapp.com",
+    projectId: "crud-js-394b8",
+    storageBucket: "crud-js-394b8.appspot.com",
+    messagingSenderId: "260132245242",
+    appId: "1:260132245242:web:6ae36a22b666d275af0343",
 };
 
-
-// Inicializa Firebase
+// Inicialización de la aplicación Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app); // Obtiene la referencia de la base de datos
 
-try {
-    await setPersistence(browserLocalPersistence);
-} catch (err) {
-    if (err.code == 'failed-precondition') {
-        // Persistencia de datos no disponible
-        console.error('Persistencia de datos no disponible');
-    } else if (err.code == 'unimplemented') {
-        // Navegador no compatible con la persistencia de Realtime Database
-        console.error('Navegador no compatible con la persistencia de Realtime Database');
-    }
-}
+// Importaciones de funciones de base de datos de Firebase
+import {
+    getDatabase,
+    set,
+    get,
+    update,
+    remove,
+    ref,
+    child,
+    push,
+    onValue
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-// Obtiene el formulario de registro por su ID
+// Obtenemos una referencia a la base de datos
+const db = getDatabase(app);
+
+// Obtenemos referencia al formulario de registro
 const registerForm = document.getElementById("register-form");
 
-// Obtiene la tabla de estudiantes por su ID
-const studentTable = document.getElementById("student-table").getElementsByTagName('tbody')[0];
-
-// Función para agregar un estudiante a la tabla
-function agregarEstudianteATabla(estudiante) {
-    const newRow = studentTable.insertRow(); // Inserta una nueva fila en la tabla
-    const cellNum = newRow.insertCell(0);
-    const cellNombre = newRow.insertCell(1);
-    const cellApellido1 = newRow.insertCell(2);
-    const cellApellido2 = newRow.insertCell(3);
-    const cellTelefono = newRow.insertCell(4);
-    const cellEmail = newRow.insertCell(5);
-    const cellOpciones = newRow.insertCell(6);
-
-    cellNum.textContent = studentTable.rows.length; // Asigna el número de fila
-    cellNombre.textContent = estudiante.nombre;
-    cellApellido1.textContent = estudiante.ape1;
-    cellApellido2.textContent = estudiante.ape2;
-    cellTelefono.textContent = estudiante.telef;
-    cellEmail.textContent = estudiante.email;
-
-    // Crea los botones de editar y eliminar
-    const btnEditar = document.createElement("button");
-    btnEditar.classList.add("btn", "btn-warning");
-    btnEditar.innerHTML = '<i class="fa fa-pencil" aria-hidden="true"></i>';
-    cellOpciones.appendChild(btnEditar);
-
-    const btnEliminar = document.createElement("button");
-    btnEliminar.classList.add("btn", "btn-danger");
-    btnEliminar.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
-    cellOpciones.appendChild(btnEliminar);
-
-    // Agrega event listeners a los botones
-    btnEditar.addEventListener("click", () => {
-        // Aquí puedes implementar la lógica para editar el estudiante
-        console.log("Editar estudiante:", estudiante);
-        // Puedes abrir un modal para editar los datos del estudiante aquí
-    });
-
-    btnEliminar.addEventListener("click", () => {
-        // Aquí puedes implementar la lógica para eliminar el estudiante
-        console.log("Eliminar estudiante:", estudiante);
-        // Puedes eliminar el estudiante de la base de datos aquí
-    });
-}
-
-// Función para registrar un nuevo estudiante
-// Función para registrar un nuevo estudiante
+// Función asincrónica para registrar un nuevo estudiante
 async function registrarNuevoEstudiante() {
+    const newStudentRef = push(ref(db, "Students"));
     try {
-        // Obtiene la referencia a la colección "Students"
-        const studentsCollectionRef = collection(db, "Students");
-
-        // Guarda los datos del formulario en la base de datos
-        await addDoc(studentsCollectionRef, {
+        await set(newStudentRef, {
             nombre: registerForm["nombre"].value,
             ape1: registerForm["apellido1"].value,
             ape2: registerForm["apellido2"].value,
@@ -110,31 +45,131 @@ async function registrarNuevoEstudiante() {
             email: registerForm["email"].value,
             desc: registerForm["descripcion"].value,
         });
-
-        // Crea un objeto con los datos del estudiante
-        const estudiante = {
-            nombre: registerForm["nombre"].value,
-            ape1: registerForm["apellido1"].value,
-            ape2: registerForm["apellido2"].value,
-            telef: registerForm["telefono"].value,
-            email: registerForm["email"].value
-        };
-
-        // Agrega una nueva fila a la tabla con los datos del estudiante
-        agregarEstudianteATabla(estudiante);
-
-        // Cierra el modal después de registrar al estudiante
-        const modal = new bootstrap.Modal(document.getElementById('registerModal'));
-        modal.hide();
+        alert("¡Nuevo estudiante registrado!");
     } catch (error) {
-        // Muestra una alerta en caso de error
         alert(error);
     }
 }
 
-
-// Añade un evento de escucha para el envío del formulario
+// Evento de escucha para el envío del formulario de registro
 registerForm.addEventListener("submit", (e) => {
-    e.preventDefault(); // Evita que el formulario se envíe normalmente
-    registrarNuevoEstudiante(); // Llama a la función para registrar un nuevo estudiante
+    e.preventDefault();
+    if(validarDatos(registerForm))
+        registrarNuevoEstudiante();
 });
+
+// Obtención de referencia al formulario de edición
+const editForm = document.getElementById("edit-form");
+
+// Evento de escucha para el envío del formulario de edición
+editForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if(validarDatos(editForm))
+        editarAlumno();
+});
+
+// Evento de escucha para cambios en los estudiantes en la base de datos
+onValue(ref(db, "Students"), alumnos => {
+    actualizarTabla(alumnos)
+})
+
+// Función para actualizar la tabla de estudiantes
+function actualizarTabla(alumnos) {
+    const tabla = document.getElementById("tabla");
+    tabla.innerHTML = "";
+    const alumnosJSON = alumnos.toJSON();
+    for(const idAlumno in alumnosJSON) {
+        const alumno = alumnosJSON[idAlumno];
+        const fila = crearFila({...alumno, id: idAlumno});
+        tabla.appendChild(fila);
+    }
+}
+
+// Función para crear un botón
+function crearBoton(tipoBoton, tipoIcono, funcionManejadora){
+    const boton = document.createElement("button");
+    boton.setAttribute("type", "button");
+    boton.classList = `btn btn-${tipoBoton}`;
+    const icono = document.createElement("i");
+    icono.setAttribute("aria-hidden", "true");
+    icono.classList = `fa fa-${tipoIcono}`;
+    boton.appendChild(icono);
+    boton.addEventListener("click", funcionManejadora);
+    return boton;
+}
+
+// Función para crear una fila en la tabla de estudiantes
+function crearFila ({nombre, ape1, ape2, telef, email, id, desc}) {
+    const fila = document.createElement("tr");
+    const campoId = document.createElement("th");    
+    campoId.setAttribute("scope", "row");
+    campoId.innerText = id;
+    fila.appendChild(campoId);
+    const campos = [nombre, ape1, ape2, telef, email];
+    for(const campo of campos) {
+        const insercion = document.createElement("td");
+        insercion.innerText = campo;
+        fila.appendChild(insercion);
+    }
+
+    const botonEditar = crearBoton("warning", "pencil", () => {
+        editForm["nombre"].value = nombre
+        editForm["apellido1"].value = ape1
+        editForm["apellido2"].value = ape2
+        editForm["telefono"].value = telef
+        editForm["email"].value = email
+        editForm["descripcion"].value = desc
+        editForm["id"].value = id
+    });
+
+    const botonEliminar = crearBoton("danger", "trash", () => eliminarAlumno(id));
+
+    botonEditar.setAttribute("data-bs-toggle","modal");
+    botonEditar.setAttribute("data-bs-target","#editarModal");
+
+    const botones = document.createElement("td");
+    botones.classList = "btns";
+    botones.appendChild(botonEditar);
+    botones.appendChild(botonEliminar);
+    fila.appendChild(botones);
+
+    return fila;
+}
+
+// Función asincrónica para eliminar un estudiante
+async function eliminarAlumno(id) {
+    remove(ref(db, "Students/" + id))
+}
+
+// Función asincrónica para editar un estudiante
+async function editarAlumno () {
+    await update(ref(db, "Students/" + editForm["id"].value), {
+        nombre: editForm["nombre"].value,
+        ape1: editForm["apellido1"].value,
+        ape2: editForm["apellido2"].value,
+        telef: editForm["telefono"].value,
+        email: editForm["email"].value,
+        desc: editForm["descripcion"].value,
+    });
+    alert("¡El estudiante se ha editado!")
+}
+
+// Función para validar los datos del formulario
+function validarDatos(form){
+    if(!/[A-ZÀ-ÿa-z-,a-z. ']{0,20}/.test(form["nombre"].value))
+        return alert("Nombre no es válido.");
+
+    if(!/[A-ZÀ-ÿa-z-,a-z. ']{0,20}/.test(form["apellido1"].value))
+        return alert("Apellido1 no es válido.");
+
+    if(!/[A-ZÀ-ÿa-z-,a-z. ']{0,20}/.test(form["apellido2"].value))
+        return alert("Apellido2 no es válido.");
+
+    if(!/(6|7)[ -]*([0-9][ -]*){8}$/.test(form["telefono"].value))
+        return alert("Telefono no es válido.");
+    
+    if(!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(form["email"].value))
+        return alert("Email no es válido.");
+    
+    return true;
+}
